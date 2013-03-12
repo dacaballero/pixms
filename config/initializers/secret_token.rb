11cf31d71d772645ad8cd1facc1b595b26420627
dacaballero <dacaballero@gmail.com>
2013-03-11 21:57:03 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixms::Application.config.secret_token = '666297d73f2946dc6816be4ee9205ee33003212bea57178a9636402895a770e1287a1545bd1f135c15e6a7d47b7c8820c0e380c60f43e4c4fab9ef3326da08f0'
