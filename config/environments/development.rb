38faeb1538b22510a2e34a1176fd8c64f7c29c6c
dacaballero <dacaballero@gmail.com>
2013-05-08 19:50:51 -0600

    stylesheet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Pixms::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   Paperclip.options[:command_path] = "C:\\ImageM"
 end
