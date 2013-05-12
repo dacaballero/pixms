9825c42a7adfeaaf0a28bd2aed366bee343934cc
dacaballero <dacaballero@gmail.com>
2013-05-12 17:23:12 -0600

    Deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require breakpoints/breakpoints
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-//= require jquery_nested_form
 //= require js/jquery-1.8.3.min
+//= require breakpoints/breakpoints
+//= require jquery_nested_form
 //= require jquery-ui/jquery-ui-1.10.1.custom.min
 //= require jquery-slimscroll/jquery.slimscroll.min
 //= require fullcalendar/fullcalendar/fullcalendar.min
@@ -37,6 +36,7 @@
 //= require chosen-bootstrap/chosen/chosen.jquery.min
 //= require jquery-tags-input/jquery.tagsinput.min
 //= require ckeditor/ckeditor
+//= require_tree .
 function initListTable(table){
 	var oTable = table.dataTable({
 		"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
