1e0e905ecc39bf764a6179af2687869eb74e0e0f
dacaballero <dacaballero@gmail.com>
2013-05-21 22:55:58 -0600

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def check_client
 
   		if not current_user.nil? and current_user.user_profile.user_profile_clients.count == 1
-  			session[:client_id] = current_user.user_profile.user_profile_clients.first.id
+  			session[:client_id] = current_user.user_profile.user_profile_clients.first.client_id
   		end
   	
   end
